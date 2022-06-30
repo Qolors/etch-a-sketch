435d7c8d3e6d009aa870358e7d085e1824076c99
qolors <cmickyg@gmail.com>
2022-06-30 17:32:22 -0400

    All Functionality complete - Added Responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const grid = document.getElementById("grid");
 const clear = document.getElementById("clear");
 const color = document.getElementById("colorOn");
+const eraser = document.getElementById("erase");
+const default_size = 16;
+const paintColor = document.getElementById("colorPicker");
+
+let theColor = '#333333';
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -15,12 +20,20 @@ function liveSlide() {
 
 // Execute background-color change
 function paint(e) {
-    if (e.type === 'mouseover' && !mouseDown) return
-    e.target.style.backgroundColor = "black";
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if (color.value === "ON") {
+        e.target.style.backgroundColor = theColor;
+    } else if (eraser.value == "ON") {
+        e.target.style.backgroundColor = "white";
+    } else {
+        e.target.style.backgroundColor = "black";
+    }
 }
 
 
-
+function paintChange(colorNew) {
+    theColor = colorNew;
+}
 
 //Grid Updates
 function gridSet(val) {
@@ -44,4 +57,56 @@ function clearGrid() {
 function refreshGrid(val) {
     clearGrid()
     gridSet(val)
+}
+
+
+//run once loaded
+window.onload = () => {
+    gridSet(default_size)
+};
+
+//button actions
+
+paintColor.onchange = (e) => paintChange(e.target.value);
+
+clear.onclick = () => {
+    var set_val = document.getElementById("Slider").value
+    refreshGrid(set_val);
+}
+color.onclick = toggleColor;
+
+eraser.onclick = toggleEraser;
+
+function toggleColor() {
+    if (color.value === "OFF") {
+        color.value = "ON";
+        if (eraser.value === "ON") {
+            eraser.value = "OFF"
+            eraser.style.backgroundColor = "#73bda8";
+            eraser.style.color = "white";
+        }
+        color.style.backgroundColor = "silver";
+        color.style.color = "black";
+    } else {
+        color.value = "OFF";
+        color.style.backgroundColor = "#73bda8";
+        color.style.color = "white";
+    };
+};
+
+function toggleEraser() {
+    if (eraser.value === "OFF") {
+        eraser.value = "ON";
+        if (color.value === "ON") {
+            color.value = "OFF"
+            color.style.backgroundColor = "#73bda8";
+            color.style.color = "white";
+        }
+        eraser.style.backgroundColor = "silver";
+        eraser.style.color = "black";
+    } else {
+        eraser.value = "OFF";
+        eraser.style.backgroundColor = "#73bda8";
+        eraser.style.color = "white";
+    };
 }
